40d0bf3838d45d8c757ebc924dd3455444975c3e
subagarw <subham.agarwal@capgemini.com>
2017-06-06 12:17:47 +0530

    Update TestDeploy.sh

--- TestDeploy.sh
+++ TestDeploy.sh
@@ -7,31 +7,26 @@ echo "download success"
 echo $USER@$PUBLIC_IP
 if [ $CONFIGURE ];
 then
-        ssh -i $KEY_PATH$KEY_NAME $USER@$PUBLIC_IP << EOF
-        sudo apt-get update
-        sudo mkdir testdeploy
-        cd testdeploy
-        sudo apt-get install java-1.8.0 -y
-        sudo apt-get install java-1.8.0-openjdk-devel -y
-        sudo apt-get remove java-1.7.0-openjdk -y
-        sudo wget http://mirrors.koehn.com/apache/tomcat/tomcat-8/v8.5.8/bin/apache-tomcat-8.5.8.tar.gz
-        sudo tar -xzvf apache-tomcat-8.5.8.tar.gz
-        sudo chmod 777 apache-tomcat-8.5.8/webapps
-        sleep 30
-        logout
-        EOF
-    sleep 30
-
-        scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
-
-        sleep 30
+ssh -i $KEY_PATH$KEY_NAME $USER@$PUBLIC_IP << EOF
+sudo apt-get update
+sudo mkdir testdeploy
+cd testdeploy
+sudo apt-get install java-1.8.0 -y
+sudo apt-get install java-1.8.0-openjdk-devel -y
+sudo apt-get remove java-1.7.0-openjdk -y
+sudo wget http://mirrors.koehn.com/apache/tomcat/tomcat-8/v8.5.8/bin/apache-tomcat-8.5.8.tar.gz
+sudo tar -xzvf apache-tomcat-8.5.8.tar.gz
+sudo chmod 777 apache-tomcat-8.5.8/webapps
+sleep 30
+logout
+EOF
+sleep 30
+scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
+sleep 30
 else
-
-        scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
-        sleep 30
+scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
+sleep 30
 fi
-
-
 ssh -i $KEY_PATH$KEY_NAME $USER@$PUBLIC_IP << EOF
 sudo apache-tomcat-8.5.8/bin/startup.sh
 logout
