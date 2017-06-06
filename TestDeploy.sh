d5c9314577620484a973354b77cc915daa4d8f13
subagarw <subham.agarwal@capgemini.com>
2017-06-06 12:40:39 +0530

    Update TestDeploy.sh

--- TestDeploy.sh
+++ TestDeploy.sh
@@ -16,19 +16,19 @@ sudo apt-get install java-1.8.0 -y
 sudo apt-get install java-1.8.0-openjdk-devel -y
 sudo apt-get remove java-1.7.0-openjdk -y
 sudo wget http://mirror.fibergrid.in/apache/tomcat/tomcat-8/v8.5.15/bin/apache-tomcat-8.5.15.tar.gz
-sudo tar -xzvf apache-tomcat-8.5.8.tar.gz
-sudo chmod 777 apache-tomcat-8.5.8/webapps
+sudo tar -xzvf apache-tomcat-8.5.15.tar.gz
+sudo chmod 777 apache-tomcat-8.5.15/webapps
 logout
 EOF
 sleep 30
-scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
+scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.15/webapps
 sleep 30
 else
 echo "false"
-scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.8/webapps
+scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.15/webapps
 sleep 30
 fi
 ssh -i $KEY_PATH$KEY_NAME $USER@$PUBLIC_IP << EOF
-sudo apache-tomcat-8.5.8/bin/startup.sh
+sudo apache-tomcat-8.5.15/bin/startup.sh
 logout
 EOF
