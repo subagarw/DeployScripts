c9d688d62a988df2f2b5a1b2e9b3a1e5ff76e5cc
subagarw <subham.agarwal@capgemini.com>
2017-06-06 14:20:45 +0530

    Update TestDeploy.sh

--- TestDeploy.sh
+++ TestDeploy.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #!/usr/local/bin/expect -f
 
-
-wget -O $ARTIFACT_PATH$ARTIFACT_NAME $ARTIFACT_URL
-echo "download success"
-
 # ssh -o 'StrictHostKeyChecking no' $USER@$PUBLIC_IP
 echo $USER@$PUBLIC_IP
 if [ $CONFIGURE ];
@@ -19,23 +15,13 @@ sudo apt-get update
 yes | sudo apt-get install oracle-java8-installer
 sudo wget http://mirror.fibergrid.in/apache/tomcat/tomcat-8/v8.5.15/bin/apache-tomcat-8.5.15.tar.gz
 sudo tar -xzvf apache-tomcat-8.5.15.tar.gz
-sudo chmod 777 apache-tomcat-8.5.15/webapps
 logout
 EOF
-sleep 30
-scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:testdeploy/apache-tomcat-8.5.15/webapps
-echo "scp successfull"
-sleep 30
-else
-echo "false"
-scp -i $KEY_PATH$KEY_NAME $ARTIFACT_PATH$ARTIFACT_NAME $USER@$PUBLIC_IP:apache-tomcat-8.5.15/webapps
-sleep 30
 fi
 ssh -i $KEY_PATH$KEY_NAME $USER@$PUBLIC_IP << EOF
 cd testdeploy
-echo "changing ownership and permission"
-sudo chown root:root apache-tomcat-8.5.15/webapps/aviva.war
-sudo chmod 744 apache-tomcat-8.5.15/bin/startup.sh
+wget -O apache-tomcat-8.5.15/webapps/$ARTIFACT_NAME $ARTIFACT_URL
+echo "download success"
 sudo apache-tomcat-8.5.15/bin/startup.sh
 logout
 EOF
